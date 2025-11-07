e349fe1162e7e9b9c02cde8839bd6156eb2c8381
sz237 <156171059+sz237@users.noreply.github.com>
2025-11-07 17:00:06 -0600

    Configure JavaMailSender in PlusOneBackendApplication
    Added JavaMailSender configuration for email functionality.

--- PlusOneBackend/src/main/java/com/plusone/PlusOneBackend/PlusOneBackendApplication.java
+++ PlusOneBackend/src/main/java/com/plusone/PlusOneBackend/PlusOneBackendApplication.java
@@ -1,7 +1,12 @@
 package com.plusone.PlusOneBackend;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;                
+import org.springframework.mail.javamail.JavaMailSender;        
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootApplication
 public class PlusOneBackendApplication {
