6f7885e492d17611e010926934268fb61fe79b19
sz237 <sarah.d.zeng@vanderbilt.edu>
2025-11-08 18:42:13 -0600

    changed local host url to our render url

--- plusone/src/services/connectionService.ts
+++ plusone/src/services/connectionService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { ConnectionRequest } from "../types/connection";
 
-const API_BASE_URL = 'http://localhost:8080/api/connections';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export interface UserProfile {
   userId: string;
